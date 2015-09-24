81269e54da89f2c5da24daa300b0aca577f213fe
Irving-cl <oos1111@sjtu.edu.cn>
2015-09-24 15:42:57 +0800

    Update OminousOmino.java

--- MySolutions/OminousOmino/OminousOmino.java
+++ MySolutions/OminousOmino/OminousOmino.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 
 public class OminousOmino 
 {
-	public static void main( String [] args )
-	{
-		Scanner input = new Scanner( System.in );
-		int numCases = input.nextInt();
+    public static void main( String [] args )
+    {
+	Scanner input = new Scanner( System.in );
+	int numCases = input.nextInt();
 		
-		boolean [] answers = new boolean[ numCases ];
-		for( int n = 0; n < numCases; n++ )
-		{
-			int X = input.nextInt();
-			int R = input.nextInt();
-			int C = input.nextInt();
-			answers[ n ] = RichardWins( X, Math.max( R, C ), Math.min( R, C ) );
-		}
-	
-		for( int n = 0; n < numCases; n++ )
-		{
-			String winner = answers[ n ] ? "RICHARD" : "GABRIEL";
-			System.out.printf( "Case #%d: %s\n", n + 1, winner );
-		}
-			
+	boolean [] answers = new boolean[ numCases ];
+	for( int n = 0; n < numCases; n++ )
+	{
+		int X = input.nextInt();
+		int R = input.nextInt();
+		int C = input.nextInt();
+		answers[ n ] = RichardWins( X, Math.max( R, C ), Math.min( R, C ) );
 	}
+	
+	for( int n = 0; n < numCases; n++ )
+	{
+		String winner = answers[ n ] ? "RICHARD" : "GABRIEL";
+		System.out.printf( "Case #%d: %s\n", n + 1, winner );
+	}
+			
+    }
 	
     private static boolean RichardWins( int X, int R, int C )
     {
